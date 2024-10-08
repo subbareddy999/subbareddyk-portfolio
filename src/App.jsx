9b724042da7e68587f057b1c7ff09bbf9aa8ca35
subbareddy999 <subba2868@gmail.com>
2024-10-08 10:57:57 +0530

    work page added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 const App = () => {
     return (
@@ -20,6 +21,7 @@ const App = () => {
                 <Hero />
                 <About />
                 <Skill />
+                <Work />
             </main>
         </>
     );
